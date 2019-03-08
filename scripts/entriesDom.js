de05fd476c737b19600fea17e738c1f2339cc016
Chris Morgan <bluemorgan2323@gmail.com>
2019-03-08 10:55:47 -0600

    Created data.js and entriesDom.js and entryComponent.js. The purpose of doing this was to modularize my functions and make my code easier to read.

---
+++ scripts/entriesDom.js
@@ -0,0 +1,16 @@
+/*
+    Modularized version of previous code. Created DOM object which defines the renderJournalEntries method. This method defines the container for allEntries in the DOM. It loops through an array of entries and adds each of them to the entryLog container / renders them to the DOM. This method is to be used with API.getEntries() which returns an array as a parsedResponse. 
+    ENTRYCOMP.makeJournalEntryComponent(entry) creates the HTML structure that will be added to entryLog. See entryComponent.js for the function expression.
+*/
+const DOM = {
+
+    renderJournalEntries: function(entriesArray) {
+    const entryLog = document.querySelector(".entryLog");
+
+    entryLog.innerHTML = "";
+    entriesArray.forEach(entry => {
+        entryLog.innerHTML += ENTRYCOMP.makeJournalEntryComponent(entry);
+    });
+
+    }
+}
--- scripts/journal.js
+++ scripts/journal.js
@@ -1,71 +1,41 @@
 //Target the submit button and assign the variable submitButton to it
 const submitButton = document.getElementById("submitButton");
-
-//Created a blank array to store journal entries in
-// const allEntries = [];
-const makeJournalEntryComponent = (journalEntry) => {
-    // Create your own HTML structure for a journal entry
-    return `
-        <div class="journalEntry--container">
-            <h3 class="journalEntry--title">${journalEntry.title}</h3>
-            <p class="journalEntry--main">${journalEntry.entry}</p>
-            <p class="journalEntry--mood">I am feeling ${journalEntry.mood}</p>
-            <h4 class="journalEntry--date">${journalEntry.date}</h4>
-        </div>
-    `
-}
-
-const renderJournalEntries = (entriesArray) => {
-    const entryLog = document.querySelector(".entryLog");
-
-    entryLog.innerHTML = "";
-    entriesArray.forEach(entry => {
-        entryLog.innerHTML += makeJournalEntryComponent(entry);
-    });
-
-};
-
-const getEntries = () => {
-    return fetch("http://localhost:8088/allEntries")
-        .then(response => response.json());
-}
-
-const postEntries = (newEntryObject) => {
-    return fetch("http://localhost:8088/allEntries", {
-            method: "POST",
-            body: JSON.stringify(newEntryObject),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
-        .then(response => response.json());
-}
-
-getEntries().then(parsedResponse => renderJournalEntries(parsedResponse));
-
-
-//enter function runs when the button is clicked, storing the values of the targetted elements in the input form at the time the button is clicked
+/*
+    Main application logic that uses the functions and objects
+    defined in the other JavaScript files.
+*/
+API.getEntries().then(parsedResponse => DOM.renderJournalEntries(parsedResponse));
+/*  
+    function runs when the button is clicked, storing the values of the targetted elements in the input form at the time the button is clicked. The overall purpose of this function is to post the new journal entry into the API. Then it will get the updated entries from the API and update the DOM with the new entry.
+*/
 submitButton.addEventListener("click", event => {
-
-    //defined variables that are set equal to the input values targetted below
+/*
+    defined variables that are set equal to the input values targetted below
+*/
     const entryDate = document.getElementById("journalDate").value;
     const entryConcepts = document.getElementById("conceptsCovered").value;
     const entryMain = document.getElementById("journalEntry").value;
     const entryMood = document.getElementById("moodSelect").value;
-
-    //Define the keys and value for a JavaScript object that
-    //represents a journal entry about what you learned today
+/*
+    Define the keys and value for a JavaScript object that
+    represents a journal entry about what you learned today
+*/
     const objectsJournalEntry = {
         title: entryConcepts,
         date: entryDate,
         entry: entryMain,
         mood: entryMood
     };
-
-    postEntries(objectsJournalEntry)
-        .then(() => getEntries())
-        .then(parsedResponse => renderJournalEntries(parsedResponse));
-
-
+/*
+    Call the postEntries() method from the API object (see data.js) which adds the new journal entry to the API
+    .then after the entry is posted, call the getEntries() method from the API object (see data.js) which gets the updated Array of all journal entries
+    .then the parsedResponse (the entry Array) is passed as an argument to the renderJournalEntries method of the DOM object, which puts the updated Array into the DOM
+*/
+    API.postEntries(objectsJournalEntry)
+        .then(() => API.getEntries())
+        .then(parsedResponse => DOM.renderJournalEntries(parsedResponse))
+/*
+    Reset the input fields
+*/
     document.getElementById("journalContainer").reset();
 });
