a1890b184f43acca75a840dbafaeaff2c85b2a78
Chris Morgan <bluemorgan2323@gmail.com>
2019-03-19 13:31:48 -0500

    made a factory function.

--- scripts/journal.js
+++ scripts/journal.js
@@ -5,6 +5,18 @@ const submitButton = document.getElementById("submitButton");
     defined in the other JavaScript files.
 */
 API.getEntries().then(parsedResponse => DOM.renderJournalEntries(parsedResponse));
+/*
+    Factory function that builds the journal entry object
+*/
+const buildEntryObject = (title, date, entry, mood) => {
+    let objectsJournalEntry = {
+        title: title,
+        date: date,
+        entry: entry,
+        mood: mood
+    };
+    return objectsJournalEntry;
+}
 /*  
     function runs when the button is clicked, storing the values of the targetted elements in the input form at the time the button is clicked. The overall purpose of this function is to post the new journal entry into the API. Then it will get the updated entries from the API and update the DOM with the new entry.
 */
@@ -16,22 +28,16 @@ submitButton.addEventListener("click", event => {
     const entryConcepts = document.getElementById("conceptsCovered").value;
     const entryMain = document.getElementById("journalEntry").value;
     const entryMood = document.getElementById("moodSelect").value;
+    if(entryMain.length > 10){
+        alert("Your entry is too long");
+    }
+ 
 /*
-    Define the keys and value for a JavaScript object that
-    represents a journal entry about what you learned today
-*/
-    const objectsJournalEntry = {
-        title: entryConcepts,
-        date: entryDate,
-        entry: entryMain,
-        mood: entryMood
-    };
-/*
-    Call the postEntries() method from the API object (see data.js) which adds the new journal entry to the API
+    Call the postEntries() method from the API object (see data.js) which adds the new journal entry to the API. The factory function buildEntryObject() is passed in as a parameter.
     .then after the entry is posted, call the getEntries() method from the API object (see data.js) which gets the updated Array of all journal entries
     .then the parsedResponse (the entry Array) is passed as an argument to the renderJournalEntries method of the DOM object, which puts the updated Array into the DOM
 */
-    API.postEntries(objectsJournalEntry)
+    API.postEntries(buildEntryObject(entryConcepts, entryDate, entryMain, entryMood))
         .then(() => API.getEntries())
         .then(parsedResponse => DOM.renderJournalEntries(parsedResponse))
 /*
